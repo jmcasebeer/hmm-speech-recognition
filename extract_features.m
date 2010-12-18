54ffb9e8c519b14a23757b23cead80b0904700a6
hsandsmark <hsandsmark@gmail.com>
2010-12-18 13:34:14 -0500

    Clean up extract_features

--- extract_features.m
+++ extract_features.m
@@ -1,31 +1,30 @@
-function framefrequencies = extract_features(sound)
+function features = extract_features(sound)
     Fs = 8000;
     framesize = 80;
     overlap = 20;
     D = 6; % Number of frequencies stored from each signal frame
 
     frames = buffer(sound, framesize, overlap);
-    w = hamming(framesize);
+    window = hamming(framesize);
     [~, N] = size(frames);
-    framefrequencies = zeros(D, N);
+    features = zeros(D, N);
 
     i = 1;
     NFFT = 2^nextpow2(framesize);
     for frame = frames
-        x = frame .* w;
-        X = fft(x, NFFT)/framesize;
+        X = fft(frame .* window, NFFT)/framesize;
         F = Fs/2*linspace(0, 1, NFFT/2 + 1);
         
         % Finding local maxima in single-sided amplitude spectrum
-        [~, peaklocations] = findpeaks(abs(X(1:(NFFT/2 + 1))), 'SORTSTR', 'descend');
+        [~, peaks] = findpeaks(abs(X(1:(NFFT/2 + 1))), 'SORTSTR', 'descend');
 
-        if isempty(peaklocations)
-            peaklocations = ones(D, 1);
-        elseif length(peaklocations) < D
-            peaklocations = padarray(peaklocations, D - length(peaklocations), 1, 'post');
+        if isempty(peaks)
+            peaks = ones(D, 1);
+        elseif length(peaks) < D
+            peaks = padarray(peaks, D - length(peaks), 1, 'post');
         end
 
-        framefrequencies(:, i) = F(peaklocations(1:D))';
+        features(:, i) = F(peaks(1:D))';
         i = i + 1;
     end
 end
