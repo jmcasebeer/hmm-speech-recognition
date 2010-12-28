3763f4a57b1361d9737ee0a86c8bf3d0c6a1d7e3
H?kon Sandsmark <hsandsmark@gmail.com>
2010-12-28 16:22:18 +0100

    Commenting out crossval

--- main.m
+++ main.m
@@ -9,18 +9,18 @@ display(unique(word_labels))
 
 vocabulary = Vocabulary;
 
-crossval('mcr', audio_signals', word_labels', 'predfun', @vocabulary.train_test, 'kfold', 5)
+% crossval('mcr', audio_signals', word_labels', 'predfun', @vocabulary.train_test, 'kfold', 5)
 
-% predicted_word_labels = vocabulary.train_test(audio_signals', word_labels', audio_signals');
-% 
-% misses = 0;
-% for i = 1:length(word_labels)
-%     fact  = word_labels(i);
-%     guess = predicted_word_labels(i);
-%     if ~isequal(fact, guess)
-%         misses = misses + 1;
-%         display(sprintf('Miss %d: Predicted %s, but was %s.', misses, char(guess), char(fact)))
-%     end
-% end
-% 
-% mcr = misses / length(word_labels)
+predicted_word_labels = vocabulary.train_test(audio_signals', word_labels', audio_signals');
+
+misses = 0;
+for i = 1:length(word_labels)
+    fact  = word_labels(i);
+    guess = predicted_word_labels(i);
+    if ~isequal(fact, guess)
+        misses = misses + 1;
+        display(sprintf('Miss %d: Predicted %s, but was %s.', misses, char(guess), char(fact)))
+    end
+end
+
+mcr = misses / length(word_labels)
